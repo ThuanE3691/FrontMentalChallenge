0c03fc5f25c687349a3eaff70083a29ad317dca4
Nguyễn Minh Thuận <20520795@ms.uit.edu.vn>
2023-09-21 11:31:57 +0700

    Add Homepage to navigate to challenges path

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -3,11 +3,13 @@ import QRComponent from "./Challenge/QR_Code_Component/Components/QRComponent";
 import ResultsSummary from "./Challenge/Results_Summary/Components/ResultsSummary";
 import NewsHomePage from "./Challenge/News_Homepage/Components/NewsHomePage";
 import NotificationsPage from "./Challenge/Notifications_Page/Components/NotificationsPage";
+import HomePage from "./Components/Homepage";
 
 const AppRouter = () => {
 	const location = useLocation();
 	return (
 		<Routes location={location} key={location.path}>
+			<Route path="/" element={<HomePage />}></Route>
 			<Route path="/QR_Component" element={<QRComponent />}></Route>
 			<Route path="/Results_Summary" element={<ResultsSummary />}></Route>
 			<Route path="/News_Homepage" element={<NewsHomePage />}></Route>
---
+++ src/Components/Homepage.js
@@ -0,0 +1,70 @@
+import QR_Screen from "../Assets/QR_Component.jpg";
+import Results_Screen from "../Assets/Results_Summary.jpg";
+import Notifications_Screen from "../Assets/Notifications_Page.jpg";
+import NewsHomePage_Screen from "../Assets/News_Homepage.jpg";
+
+import { Link } from "react-router-dom";
+
+const ChallengeBox = ({ link_challenge, img_challenge, name_challenge }) => {
+	return (
+		<li className="px-4 pt-2 pb-6 transition-all duration-200 bg-white rounded-lg shadow-xl cursor-pointer w-fit hover:opacity-70">
+			<Link
+				to={`/${link_challenge}`}
+				className="grid grid-rows-[auto_256px] grid-cols-[minmax(0,256px)] gap-y-4"
+			>
+				<div className="font-medium ">{name_challenge}</div>
+				<img
+					src={img_challenge}
+					alt=""
+					className="object-cover w-full h-full rounded-md border-[1px] border-gray-200"
+				/>
+			</Link>
+		</li>
+	);
+};
+
+const Homepage = () => {
+	const challenges = [
+		{
+			link_challenge: "QR_Component",
+			img_challenge: QR_Screen,
+			name_challenge: "QR Component",
+		},
+		{
+			link_challenge: "Results_Summary",
+			img_challenge: Results_Screen,
+			name_challenge: "Results Summary",
+		},
+		{
+			link_challenge: "News_Homepage",
+			img_challenge: NewsHomePage_Screen,
+			name_challenge: "News Homepage",
+		},
+		{
+			link_challenge: "Notifications_Page",
+			img_challenge: Notifications_Screen,
+			name_challenge: "Notifications Page",
+		},
+	];
+
+	return (
+		<div className="flex flex-col items-center justify-center w-full h-auto py-8 overflow-auto lg:h-full md:px-4 lg:block bg-notification-light-gray-blue-1">
+			<p className="mb-8 text-3xl font-semibold lg:ml-12 font-outfit">
+				Challenges
+			</p>
+			<ol className="grid text-xl lg:mt-8 lg:ml-12 lg:grid-cols-4 md:grid-cols-2 font-outfit gap-y-8 gap-x-4">
+				{challenges.map(({ link_challenge, img_challenge, name_challenge }) => {
+					return (
+						<ChallengeBox
+							link_challenge={link_challenge}
+							img_challenge={img_challenge}
+							name_challenge={name_challenge}
+						></ChallengeBox>
+					);
+				})}
+			</ol>
+		</div>
+	);
+};
+
+export default Homepage;
