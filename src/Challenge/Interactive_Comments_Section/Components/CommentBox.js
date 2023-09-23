9f927154c4fd65cb4b0b5510e8774bdf426224e3
Nguyễn Minh Thuận <20520795@ms.uit.edu.vn>
2023-09-23 16:59:36 +0700

    Add UI for Replies Area, for mobile and desktop responsive

--- src/Challenge/Interactive_Comments_Section/Components/CommentBox.js
+++ src/Challenge/Interactive_Comments_Section/Components/CommentBox.js
@@ -1,32 +1,57 @@
-import amyrobson from "../Assets/avatars/image-amyrobson.png";
+import { useState } from "react";
 
-const CommentBox = () => {
+const CommentBox = ({ comment, commentType }) => {
+	const [score, setScore] = useState(comment.score);
+	const handleInAndDecsScore = (sign) => {
+		switch (sign) {
+			case "PLUS":
+				setScore((prev) => prev + 1);
+				break;
+			case "MINUS":
+				if (score >= 1) setScore((prev) => prev - 1);
+				break;
+			default:
+				break;
+		}
+	};
+
 	return (
-		<section className="grid md:grid-rows-[auto_auto] grid-rows-[auto_auto_auto] grid-cols-[auto_repeat(3,1fr)] md:w-[723px] bg-white rounded-xl px-6 py-5 gap-x-4 gap-y-4">
+		<section
+			className={`grid md:grid-rows-[auto_auto] overflow-hidden grid-rows-[auto_auto_auto] grid-cols-[auto_repeat(3,1fr)] bg-white rounded-xl px-6 py-5 gap-x-4 gap-y-4 ${
+				commentType === "reply"
+					? "col-start-2 md:w-[calc(723px-82.5px)]"
+					: "md:w-[723px]"
+			}`}
+		>
 			<div className="flex-row self-start col-span-1 row-start-3 px-3 py-2 md:px-3 md:py-4 md:col-span-1 md:row-span-2 md:flex-col flex__center bg-comments-page-Very-light-gray rounded-xl">
 				<svg
 					width="11"
 					height="11"
-					className="md:mb-4 md:mr-0 mr-4 transition-colors duration-200 cursor-pointer fill-current text-[#C5C6EF] hover:text-comments-page-Moderate-blue"
+					onClick={() => handleInAndDecsScore("PLUS")}
+					className="md:mb-4 md:mr-0 mr-6 transition-colors duration-200 cursor-pointer fill-current text-[#C5C6EF] hover:text-comments-page-Moderate-blue"
 				>
 					<path d="M6.33 10.896c.137 0 .255-.05.354-.149.1-.1.149-.217.149-.354V7.004h3.315c.136 0 .254-.05.354-.149a.484.484 0 0 0 .148-.354V5.272a.483.483 0 0 0-.148-.354.483.483 0 0 0-.354-.149H6.833V1.4a.483.483 0 0 0-.149-.354.483.483 0 0 0-.354-.149H4.915a.483.483 0 0 0-.354.149c-.1.1-.149.217-.149.354v3.37H1.08a.483.483 0 0 0-.354.15.48.48 0 0 0-.149.353v1.23c0 .136.05.254.149.353.1.1.217.149.354.149h3.333v3.39c0 .136.05.254.15.353.098.1.216.149.353.149H6.33Z" />
 				</svg>
-				<p className="mr-4 font-medium md:mb-4 md:mr-0 text-comments-page-Moderate-blue">
-					12
+				<p
+					unselectable="on"
+					className="flex items-center justify-center w-2 mr-6 font-medium text-center md:mb-4 md:mr-0 text-comments-page-Moderate-blue"
+				>
+					{score}
 				</p>
 				<svg
 					width="11"
 					height="3"
+					onClick={() => handleInAndDecsScore("MINUS")}
 					className="transition-colors duration-200 cursor-pointer fill-current text-[#C5C6EF] hover:text-comments-page-Moderate-blue"
 				>
 					<path d="M9.256 2.66c.204 0 .38-.056.53-.167.148-.11.222-.243.222-.396V.722c0-.152-.074-.284-.223-.395a.859.859 0 0 0-.53-.167H.76a.859.859 0 0 0-.53.167C.083.437.009.57.009.722v1.375c0 .153.074.285.223.396a.859.859 0 0 0 .53.167h8.495Z" />
 				</svg>
 			</div>
 			<header className="flex items-center col-span-4 md:col-span-2">
-				<img src={amyrobson} alt="" className="w-8 h-8 mr-4" />
-				<p className="font-medium">amyrobson</p>
+				<img src={comment.user.image.png} alt="" className="w-8 h-8 mr-4" />
+				<p className="font-medium">{comment.user.username}</p>
 				<div className="ml-4 mr-auto text-comments-page-Grayish-Blue">
-					1 month ago
+					{comment.createdAt}
 				</div>
 			</header>
 			<div className="flex items-center self-center col-start-4 row-start-3 font-medium cursor-pointer md:self-start md:col-start-4 md:row-start-1 md:mt-1 place-self-end text-comments-page-Moderate-blue hover:text-comments-page-Light-grayish-blue">
@@ -36,9 +61,12 @@ const CommentBox = () => {
 				<p className=" color__trans">Reply</p>
 			</div>
 			<p className="col-span-4 col-start-1 md:col-span-3 md:col-start-2 text-comments-page-Grayish-Blue">
-				Impressive! Though it seems the drag feature could be improved. But
-				overall it looks incredible. You've nailed the design and the
-				responsiveness at various breakpoints works really well.
+				{commentType === "reply" && (
+					<span className="font-medium cursor-pointer text-comments-page-Moderate-blue color_trans hover:text-comments-page-Light-grayish-blue">
+						@{comment.replyingTo + " "}
+					</span>
+				)}
+				{comment.content}
 			</p>
 		</section>
 	);
--- src/Challenge/Interactive_Comments_Section/Components/Comments_Page.js
+++ src/Challenge/Interactive_Comments_Section/Components/Comments_Page.js
@@ -1,10 +1,44 @@
 import CommentBox from "./CommentBox";
+import data from "../Data/data";
+
+const RepliesArea = ({ replies }) => {
+	return (
+		<div
+			className={`grid grid-cols-[auto_auto] gap-y-4 md:gap-x-10 gap-x-4 `}
+			style={{ gridTemplateRows: `repeat(${replies.length},auto)` }}
+		>
+			<div className=" w-[2.5px] md:h-[97%] h-full bg-comments-page-Light-gray rounded row-span-full md:ml-10"></div>
+			{replies.map((reply) => {
+				return (
+					<CommentBox
+						comment={reply}
+						key={reply.id + "-" + reply.user.username}
+						commentType="reply"
+					></CommentBox>
+				);
+			})}
+		</div>
+	);
+};
 
 const Comments_Page = () => {
 	return (
-		<div className="flex justify-center w-full h-full overflow-auto font-Rubik md:h-full bg-comments-page-Very-light-gray">
-			<main class="w-fit h-fit md:my-16 md:mx-0 mx-4 my-8">
-				<CommentBox></CommentBox>
+		<div className="flex justify-center w-full h-auto overflow-auto font-Rubik md:h-full bg-comments-page-Very-light-gray">
+			<main className="grid mx-4 my-8 w-fit h-fit md:my-16 md:mx-0 gap-y-4">
+				{data.comments.map((comment) => {
+					return (
+						<>
+							<CommentBox
+								comment={comment}
+								key={comment.id + "-" + comment.user.username}
+								commentType="comment"
+							></CommentBox>
+							{comment.replies.length > 0 && (
+								<RepliesArea replies={comment.replies}></RepliesArea>
+							)}
+						</>
+					);
+				})}
 			</main>
 		</div>
 	);
